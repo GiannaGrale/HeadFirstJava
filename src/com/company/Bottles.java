81f27cf6046f82484a901f2ba128447601d16a6c
Anna <giannagrale@gmail.com>
2021-09-26 15:11:46 +0300

    Initial commit

---
+++ src/com/company/Bottles.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Bottles {
+
+    public static void main(String[] args) {
+        int beerNum = 3;
+        String word = "bottles";
+
+        while (beerNum > 0) {
+            System.out.println(beerNum + " " + word + " of beer on the wall");
+            System.out.println(beerNum + " " + word + " of beer");
+            System.out.print("Take one down. ");
+            System.out.println("Pass it around");
+            beerNum--;
+
+            if (beerNum == 1) {
+                word = "bottle";
+            } else if (beerNum == 0) {
+                System.out.println("No bottles on the wall!");
+            }
+        }
+    }
+}
